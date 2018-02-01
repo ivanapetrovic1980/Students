38c5cf86cce586c1e1227a8711a8f98638f08cda
Frontend <frontend@Cubess-iMac-7.local>
2018-02-01 15:22:19 +0100

    changed student to forwarded

--- src/app/mock-student.ts
+++ src/app/mock-student.ts
@@ -8,7 +8,7 @@ export const STUDENTS: Student[] = [
   { id: 4, name: 'Nikola Dosenovic', gender: 'male', description: 'konfliktan'},
   { id: 5, name: 'Borivoje Skrbina', gender: 'male', description: 'expert'},
   { id: 6, name: 'Anka Markovic', gender: 'female', description: 'alone'},
-  { id: 7, name: 'Ivana Petrovic', gender: 'female', description: 'confused'}
+  { id: 7, name: 'Ivana Petrovic', gender: 'female', description: 'confused'},
   { id: 8, name: 'Jovana Nikolic' gender: 'female', description: 'volunteer'},
   { id: 9, name: 'Luka Cosic' gender: 'male', description: 'finished'},
   { id: 10, name: 'Marija Jeremic' gender: 'female', description: 'back'}
